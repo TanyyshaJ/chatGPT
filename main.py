f722b0ad047394bff54d35572a6132aac0c5cad3
TanyyshaJ <tanisha109@gmail.com>
2023-08-12 11:26:26 +0530

    main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openai
 
 
-openai.api_key = "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"
+openai.api_key = "OPENAI_KEY"
 
 completion = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages=[{"role":"user", "content":"Write an essay on Penguins"}])
 
